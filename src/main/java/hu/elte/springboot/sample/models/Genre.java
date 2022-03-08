613e6d36cf4ab56d3a1f0dedf6d325e694c0e0a5
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-03-08 15:53:02 +0100

    create genre model and schema

---
+++ src/main/java/hu/elte/springboot/sample/models/Genre.java
@@ -0,0 +1,73 @@
+package hu.elte.springboot.sample.models;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+    @Enumerated(EnumType.STRING)
+    private Style style;
+
+    public enum Style {
+        primary,
+        secondary,
+        success,
+        danger,
+        warning,
+        info,
+        light,
+        dark
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+}

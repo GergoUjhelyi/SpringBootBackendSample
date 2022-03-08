43fbd5f86660e9e7d4d341f7c994125b8f243caf
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-03-08 14:42:23 +0100

    Initial commit

---
+++ src/main/java/hu/elte/springboot/sample/SpringBootBackendSampleApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.springboot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/elte/springboot/sample/SpringBootBackendSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.springboot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

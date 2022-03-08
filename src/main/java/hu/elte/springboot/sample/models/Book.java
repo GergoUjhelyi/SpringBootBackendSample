b4c1d139d8a5b06a4cb4c101c8f68840a4cc1093
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-03-08 15:47:35 +0100

    create book schema

--- src/main/java/hu/elte/springboot/sample/models/Book.java
+++ src/main/java/hu/elte/springboot/sample/models/Book.java
@@ -1,4 +1,140 @@
 package hu.elte.springboot.sample.models;
 
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    private String authors;
+
+    @Nullable
+    private String description;
+
+    private Date released_at;
+
+    @Nullable
+    private String cover_image;
+
+    private int pages;
+
+    @Column(length = 3)
+    private String language_code = "hu";
+
+    @Column(length = 13, unique = true)
+    private String isbn;
+
+    private int in_stock;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Nullable String description) {
+        this.description = description;
+    }
+
+    public Date getReleased_at() {
+        return released_at;
+    }
+
+    public void setReleased_at(Date released_at) {
+        this.released_at = released_at;
+    }
+
+    @Nullable
+    public String getCover_image() {
+        return cover_image;
+    }
+
+    public void setCover_image(@Nullable String cover_image) {
+        this.cover_image = cover_image;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getLanguage_code() {
+        return language_code;
+    }
+
+    public void setLanguage_code(String language_code) {
+        this.language_code = language_code;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getIn_stock() {
+        return in_stock;
+    }
+
+    public void setIn_stock(int in_stock) {
+        this.in_stock = in_stock;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
